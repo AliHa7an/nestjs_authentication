c18290d04a5f25980b475eaedc91cc85efa9cb6d
Ali_Ha7an <ali00hassan969@gmail.com>
2021-02-15 12:12:27 +0500

    added all files

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -6,27 +6,71 @@ import { User } from '../../modules/users/user.entity';
 import { Post } from '../../modules/posts/post.entity';
 
 export const databaseProviders = [
-    {
-        provide: SEQUELIZE,
-        useFactory: async () => {
-            let config;
-            switch (process.env.NODE_ENV) {
-                case DEVELOPMENT:
-                    config = databaseConfig.development;
-                    break;
-                case TEST:
-                    config = databaseConfig.test;
-                    break;
-                case PRODUCTION:
-                    config = databaseConfig.production;
-                    break;
-                default:
-                    config = databaseConfig.development;
-            }
-            const sequelize = new Sequelize(config);
-            sequelize.addModels([User, Post]);
-            await sequelize.sync();
-            return sequelize;
-        },
+  {
+    provide: SEQUELIZE,
+    useFactory: async () => {
+      let config;
+      switch (process.env.NODE_ENV) {
+        case DEVELOPMENT:
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
+      }
+      //       config = {
+      //         username: 'postgres',
+      //         password: '123',
+      //         database: 'test3',
+      //         host: 'localhost',
+      //         port: '5432',
+      //         dialect: 'postgres',
+      //       };
+      //       console.log(config);
+
+      //       const sequelize = new Sequelize({
+      //         ...config,
+      //         dialectOptions: {
+      //           statement_timeout: 1000,
+      //           idle_in_transaction_session_timeout: 5000,
+      //           requestTimeout: 3000,
+      //           options: {
+      //             requestTimeout: 3000,
+      //           },
+      //         },
+      //         pool: {
+      //           max: 10,
+      //           min: 0,
+      //           idle: 10000,
+      //         },
+      //       });
+      //       await sequelize.authenticate();
+      //       sequelize.addModels([User, Post]);
+      //       try {
+      //         await sequelize.sync({
+      //           alter: true,
+      //           logging: console.log,
+      //         });
+      //         console.log(' sequelize setup successfuly.');
+      //       } catch (Err) {
+      //         console.log(Err);
+      //       }
+
+      //       const x = await User.findAll();
+
+      //       return sequelize;
+      //     },
+      //   },
+      // ];
+      const sequelize = new Sequelize(config);
+      sequelize.addModels([User, Post]);
+      await sequelize.sync();
+      return sequelize;
     },
+  },
 ];
